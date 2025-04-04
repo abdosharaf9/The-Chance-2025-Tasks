1a5b11f8cf7758b15d0d1534e28cab1c6ce7dfcd
Abdo Sharaf <bodyasharaf8@gmail.com>
2025-04-04 17:40:02 +0200

    add descriptive comments to Sudoku validation functions

--- src/week_1/sudoko/SudokuChecker.kt
+++ src/week_1/sudoko/SudokuChecker.kt
@@ -2,6 +2,17 @@ package week_1.sudoko
 
 import kotlin.math.sqrt
 
+//fun isSudokuGridValid(grid: List<List<Char>>): Boolean {
+//    return false
+//}
+
+/**
+ * Validate if a given Sudoku board is a valid board or not.
+ *
+ * @param grid The Sudoku board you need to validate
+ *
+ * @return Returns true if the board is a valid Sudoku board, and false if not.
+ * */
 fun isSudokuGridValid(grid: List<List<Char>>): Boolean {
     if (grid.isEmpty()) {
         return false
@@ -56,6 +67,15 @@ fun isSudokuGridValid(grid: List<List<Char>>): Boolean {
     return true
 }
 
+/**
+ * Validate if a given list of cell contains duplicates or not, and check if all
+ * the cell values are in the given values range.
+ *
+ * @param cells The list of cells you want to validate
+ * @param cellRange Set of allowed numbers or letters values
+ *
+ * @return Returns true if the list doesn't contain duplicates, and false if not.
+ * */
 private fun areCellsValid(cells: List<Char>, cellRange: Set<Char>): Boolean {
     val seen = mutableSetOf<Char>()
 
